e266530cfb8e0635df49c9006a3ffd1ea0442641
Preston Watson <preston.watson89@gmail.com>
2016-04-08 09:02:57 -0500

    adding images and javascript files. adding finalized css files.

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+
+// User chooses either Cap or Ironman
+      //   if user chooses Cap
+    //       users icon will be cap
+      //   if user chooses Ironman
+    //       users icon will be ironman
+// User Clicks on square and X or O shows up
+  // if user clicks on square,
+  //   O shows up on square
+// User has 3 objects in a row and wins
+  // if user has three Os in a row
+  //   user has won
+// user clicks on a space taken and cant add their objects
+    // if user clicks on square with an x or o
+// user clicks button to start new game
+    if user clicks on reset button, begins new game
+
